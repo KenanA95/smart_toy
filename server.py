c63b6f8da8040624d3f4812e171079cac2892d2d
Kenan Alkiek <kenan.r.alkiek@gmail.com>
2017-08-31 10:45:29 -0400

    Function to run http server

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import http.server
 
+PORT = 8000
+
 
 class ToyHandler(http.server.BaseHTTPRequestHandler):
 
@@ -8,3 +10,13 @@ class ToyHandler(http.server.BaseHTTPRequestHandler):
 
     def do_POST(self):
         print("In POST function....")
+
+
+def run(server_class=http.server.HTTPServer, handler_class=ToyHandler):
+
+    server_address = ('', PORT)
+    httpd = server_class(server_address, handler_class)
+
+    print("Serving PORT: ", PORT)
+    httpd.serve_forever()
+
