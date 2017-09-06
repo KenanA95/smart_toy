26ade78ec360b722303ed20785cf59db6b98fdc4
Kenan <kenan.r.alkiek@gmail.com>
2017-09-05 21:09:52 -0400

    Initial commit

---
+++ record_route.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+
+class Record:
+    def __init__(self):
+        self.state = False
+
+    def set_state(self, val):
+        self.state = val
+
+    def get_state(self):
+        return self.state
+
+
+# Helper Functions
+
+def init_db():
+    # Store route in database file
+    conn = sqlite3.connect('route.db')
+
+    # Initialize data table
+    conn.execute('''CREATE TABLE IF NOT EXISTS routes
+                        (command text, value int, timestamp date)''')
+
+    # Clear any previous data
+    cur = conn.cursor()
+    cur.execute('delete from routes')
+    conn.commit()
+
+    return conn
+
+
+def insert_route(conn, command, val, timestamp):
+    cur = conn.cursor()
+    cur.execute("insert into routes (command, value, timestamp) values (?, ?, ?)", (command, val, timestamp))
+    conn.commit()
+
+
+def output_routes(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM routes")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+def repeat_route(conn):
+    return
