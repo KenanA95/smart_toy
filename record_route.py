988475d183554f4cb6c51f80be34910f91f5b694
Kenan <kenan.r.alkiek@gmail.com>
2017-09-06 10:32:37 -0400

    Completed repeat route function

--- record_route.py
+++ record_route.py
@@ -1,4 +1,6 @@
 import sqlite3
+from datetime import datetime
+import time
 
 
 class Record:
@@ -11,8 +13,6 @@ class Record:
     def get_state(self):
         return self.state
 
-
-# Helper Functions
 
 def init_db():
     # Store route in database file
@@ -46,4 +46,34 @@ def output_routes(conn):
 
 
 def repeat_route(conn):
-    return
+    # Pull all records
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM routes")
+    rows = cur.fetchall()
+
+    for index, row in enumerate(rows):
+
+        if row[0] == 'stop':
+            print("Completed the recorded route...")
+            return
+
+        command, val = row[0], row[1]
+
+        # Figure out how long to wait until sending off the next command
+        start_time = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S.%f')
+        stop_time = datetime.strptime(rows[index + 1][2], '%Y-%m-%d %H:%M:%S.%f')
+        duration = (stop_time - start_time).total_seconds()
+
+        send_to_hardware(command, val)
+        time.sleep(duration)
+
+
+# Forward the message along to the hardware side. For now just print out a message to console
+def send_to_hardware(command, val):
+
+    if command == 'fb':
+        print("Forward/Backward Slider: ", val)
+    elif command == 'lr':
+        print("Left/Right Slider: ", val)
+    else:
+        print("Invalid Command: ", command)
--- server.py
+++ server.py
@@ -7,7 +7,6 @@ from datetime import datetime
 record = Record()
 
 
-#
 class ToyHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -17,49 +16,11 @@ class ToyHandler(http.server.BaseHTTPRequestHandler):
 
     def do_POST(self):
         self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
         length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(length).decode('utf-8')
         handle_request(post_data)
-
-
-# TODO: Refactor
-def handle_request(post_data):
-
-    # Otherwise its a button request to record a route
-    if len(post_data.split('=')) == 1:
-        if post_data == 'start':
-            print("Start recording selected...")
-            record.set_state(True)
-            # Clear the current table
-            cur = conn.cursor()
-            cur.execute('delete from routes')
-            conn.commit()
-
-        elif post_data == 'stop':
-            record.set_state(False)
-            print("Stop recording selected. Current routes saved...")
-            output_routes(conn)
-            # repeat_route(conn)
-
-    # If it has an equals sign its a slider request
-    else:
-        command, val = post_data.split('=')
-        send_to_hardware(command, val)
-
-        if record.get_state():
-            print("Inserting route...")
-            insert_route(conn, command, val, datetime.now())
-
-
-# Forward the message along to the hardware side. For now just print out a message to console
-def send_to_hardware(command, val):
-
-    if command == 'fb':
-        print("Forward/Backward Slider:  ", val)
-    elif command == 'lr':
-        print("Left/Right Slider ", val)
-    else:
-        print("Invalid Command: ", command)
 
 
 def run_server(port, server_class=http.server.HTTPServer, handler_class=ToyHandler):
@@ -68,6 +29,40 @@ def run_server(port, server_class=http.server.HTTPServer, handler_class=ToyHandl
     httpd = server_class(server_address, handler_class)
     print("Serving PORT: ", port)
     httpd.serve_forever()
+
+
+# Parse the POST and fire off a function based on the command
+def handle_request(post_data):
+
+    # If it has an equals sign its a slider request for forward/back/left/right
+    if len(post_data.split('=')) > 1:
+        command, val = post_data.split('=')
+        send_to_hardware(command, val)
+
+        # If we're in record mode store the command in the db
+        if record.get_state():
+            print("Inserting route into db...")
+            insert_route(conn, command, val, datetime.now())
+
+    # Otherwise its a button request to start/stop/play a recorded route
+    else:
+        if post_data == 'start':
+            print("Start recording selected...")
+            # Clear any previous routes from the table
+            cur = conn.cursor()
+            cur.execute('delete from routes')
+            conn.commit()
+            record.set_state(True)
+
+        elif post_data == 'stop':
+            print("Stop recording selected. Current routes saved...")
+            output_routes(conn)
+            insert_route(conn, command="stop", val=0, timestamp=datetime.now())
+            record.set_state(False)
+
+        elif post_data == 'play':
+            print("Play selected. Now repeating the saved route...")
+            repeat_route(conn)
 
 
 conn = init_db()
