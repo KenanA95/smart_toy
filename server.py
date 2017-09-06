0f58ba25d5156accb8974284b7d5f2ccec4df7ff
Kenan <kenan.r.alkiek@gmail.com>
2017-09-05 21:05:42 -0400

    Record route requests in sqlite3 db

--- server.py
+++ server.py
@@ -1,35 +1,63 @@
 import http.server
+from record_route import *
+from datetime import datetime
 
 
+# State variable to keep track if the route requests should be recorded for playback
+record = Record()
+
+
+#
 class ToyHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        print("In GET function....")
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_POST(self):
-        print("In POST function....")
         self.send_response(200)
         length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(length).decode('utf-8')
-        forward_command(post_data)
+        handle_request(post_data)
+
+
+# TODO: Refactor
+def handle_request(post_data):
+
+    # Otherwise its a button request to record a route
+    if len(post_data.split('=')) == 1:
+        if post_data == 'start':
+            print("Start recording selected...")
+            record.set_state(True)
+            # Clear the current table
+            cur = conn.cursor()
+            cur.execute('delete from routes')
+            conn.commit()
+
+        elif post_data == 'stop':
+            record.set_state(False)
+            print("Stop recording selected. Current routes saved...")
+            output_routes(conn)
+            # repeat_route(conn)
+
+    # If it has an equals sign its a slider request
+    else:
+        command, val = post_data.split('=')
+        send_to_hardware(command, val)
+
+        if record.get_state():
+            print("Inserting route...")
+            insert_route(conn, command, val, datetime.now())
 
 
 # Forward the message along to the hardware side. For now just print out a message to console
-def forward_command(post_data):
-    # Parse out the command and value from the POST
-    command, val = post_data.split('=')
+def send_to_hardware(command, val):
 
-    if command == 'f':
-        print("Forward ", val)
-    elif command == 'b':
-        print("Backward ", val)
-    elif command == 'l':
-        print("Left  ", val)
-    elif command == 'r':
-        print("Right  ", val)
+    if command == 'fb':
+        print("Forward/Backward Slider:  ", val)
+    elif command == 'lr':
+        print("Left/Right Slider ", val)
     else:
         print("Invalid Command: ", command)
 
@@ -41,4 +69,6 @@ def run_server(port, server_class=http.server.HTTPServer, handler_class=ToyHandl
     print("Serving PORT: ", port)
     httpd.serve_forever()
 
+
+conn = init_db()
 run_server(port=8000)
