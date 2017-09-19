Fixed scaling function

server.py
@@ -3,6 +3,8 @@ from record_route import *
 from datetime import datetime
 from test_robot import Robot
 import json
+import numpy as np
+from scipy.interpolate import interp1d
 
 
 # State variable to keep track if the route requests should be recorded for playback
@@ -27,25 +29,16 @@ class ToyHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_POST(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
         self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'text/html')
+        self.end_headers()
         length = int(self.headers['Content-Length'])
         value = json.loads(self.rfile.read(length).decode('utf-8'))
         handle_request(self.path, int(value['value']))
-        self.end_headers()
-
-
-def run_server(port):
-
-    server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, ToyHandler)
-    print("Serving PORT: ", port)
-    httpd.serve_forever()
 
 
 def handle_request(path, value):
-    path = path.strip()
 
     if path == '/forwardBackward' or path == '/leftRight':
         send_to_hardware(command=path, val=value)
@@ -73,19 +66,27 @@ def handle_request(path, value):
         repeat_route(conn)
 
 
+def run_server(port):
+
+    server_address = ('', port)
+    httpd = http.server.HTTPServer(server_address, ToyHandler)
+    print("Serving PORT: ", port)
+    httpd.serve_forever()
+
+
 # Parse and forward the command from the UI to the robot
-# Value from the front-end slider comes in the range 0-100 where 1-49 is backwards 50 is neutral
+# Value from the front-end slider comes in the range 0-100 where 0-49 is backwards 50 is neutral
 # and 51-100 is forward. This value is mapped to the robot in the range of 0-100
 def send_to_hardware(command, val):
 
     if command == '/forwardBackward':
-        if 0 < val < 50:
-            val = scale_value(val, current_range=49)
+        if 0 <= val < 50:
+            val = scale_backward_value(val)
             robot.go_backward(val)
         elif val == 50:
             robot.stop()
         elif val > 50:
-            val = scale_value(val, current_range=99)
+            val = scale_forward_value(val)
             robot.go_forward(val)
 
     elif command == '/leftRight':
@@ -97,33 +98,41 @@ def send_to_hardware(command, val):
             robot.turn_right()
 
 
-# Map to 1-100 range
-def scale_value(value, current_range):
-    new_range = 99
-    scaled_value = (((value-1) * new_range) / current_range) + 1
-    return math.ceil(scaled_value)
+# Map from 51-100 to 1-100 range
+def scale_forward_value(value):
+    f = interp1d([51, 100], [1, 100])
+    return int(np.round(f(value)))
+
+
+# Map from 0-49 to 1-100 range where 0 is the fastest and 49 is the slowest
+def scale_backward_value(value):
+    f = interp1d([0, 49], [100, 1])
+    return int(np.round(f(value)))
 
 
 def repeat_route(conn):
-    # Pull all records
+    # Pull all records from the file
     cur = conn.cursor()
     cur.execute("SELECT * FROM routes")
     rows = cur.fetchall()
 
     for index, row in enumerate(rows):
-        if row[0] == 'stop':
+        # Get the command sent by the user and its associated value
+        command, val = row[0], row[1]
+
+        # Reached the last command
+        if command == 'stop':
             print("Completed the recorded route...")
             return
 
-        command, val = row[0], row[1]
-
-        # Figure out how long to wait until sending off the next command
-        start_time = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S.%f')
-        stop_time = datetime.strptime(rows[index + 1][2], '%Y-%m-%d %H:%M:%S.%f')
-        duration = (stop_time - start_time).total_seconds()
-
-        send_to_hardware(command, val)
-        time.sleep(duration)
+        else:
+            # Figure out how long to wait until sending off the next command
+            start_time = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S.%f')
+            stop_time = datetime.strptime(rows[index + 1][2], '%Y-%m-%d %H:%M:%S.%f')
+            duration = (stop_time - start_time).total_seconds()
+            send_to_hardware(command, val)
+            # Wait the original amount of time as the command before a different command was sent
+            time.sleep(duration)
 
 
 conn = init_db()